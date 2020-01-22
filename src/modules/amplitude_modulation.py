1d44e09fa9126a1eb800a5437274aee22945b8f7
Ljqd <egor.moloch@gmail.com>
2020-01-22 12:38:43 +0200

    Modified AM

--- src/modules/amplitude_modulation.py
+++ src/modules/amplitude_modulation.py
@@ -8,7 +8,13 @@ def am_algorithm(audio, carrier):
     samples = len(audio[1])
     result = np.linspace(0, samples / sample_rate, samples) # time
 
-    audio_amlitude = max(audio[1])
+    temp1 = max(audio[1])
+    temp2 = abs(min(audio[1]))
+    if temp1 > temp2:
+        audio_amlitude = temp1
+    else:
+        audio_amlitude = temp2
+
     for i in range(len(result)):
         result[i] = carrier[result[i]] * (
             1 + const.amplitude_coef() * audio[1][i] / audio_amlitude
